e0a36ca0af3ed7b4f157502142a053d5757daf7e
dor300s <dor300s@gmail.com>
2020-03-30 20:58:14 +0300

    Portfolio v1.0

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function createProjects() {
     gProjects.push(createProject(3, 'Game of life', 'Game of life', 'Game - the cells change position every few seconds', "projects/Ex-game-of-life/index.html", '30/03/2020', ['game, board']));
     gProjects.push(createProject(4, 'Ball board', 'Ball board', 'Game - the user need to collect the balls', "projects/ball-board-start-here/index.html", '30/03/2020', ['game, board']));
     gProjects.push(createProject(5, 'Pacman', 'Pacman', 'Game - the user need to eat all the dots without getting cougth by the ghosts', "projects/pacman-TODOS/index.html", '30/03/2020', ['game, board']));
-    gProjects.push(createProject(6, 'Mine Sweeper', 'Mine Sweeper', 'Game - the user need to open all the num cells and mark the mines with flags', "projects/MineSweeper/index.html", '30/03/2020', ['game, timer', 'board']));
+    gProjects.push(createProject(6, 'Mine Sweeper', 'Mine Sweeper', 'Game - the user need to open all the num cells and mark the mines with flags', "projects/MineSweeper/index.html", '30/03/2020', ['game, timer, board']));
     gProjects.push(createProject(7, 'Todo list', 'Todo list', 'Tool - the user can make and manage list of tasks', "projects/proj-todo-mvc/index.html", '30/03/2020', ['tasks tool, board']));
     gProjects.push(createProject(8, 'Book shop', 'Book shop', 'The user can manage books for book shop', "projects/Ex.Book-shop/index.html", '30/03/2020', ['manage books shop, board']));
 
